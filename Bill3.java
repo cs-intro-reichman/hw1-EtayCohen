edcf9e5bc755d819fa6546e898ba3f015621f2c4
Etay Cohen <etay@altahq.com>
2024-11-08 13:59:33 +0200

    completed homework

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,13 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+
+		double bill = Double.parseDouble(args[3]);
+		double amount = Math.ceil(bill / 3);
+
+		System.out.println(String.format("Dear %s, %s, and %s: pay %.1f Shekels each.", name1, name2, name3, amount));
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,14 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
+	public static void main(String[] args) {
+		int currentSavings = Integer.parseInt(args[0]);
+		double intrestRate = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+
+		int futureValue = (int) (currentSavings * Math.pow(1 + (intrestRate / 100), years));
+
+		System.out.println(String.format("After %d years, $%d saved at %.1f%% will yield $%d ", years, currentSavings,
+				intrestRate, futureValue));
+
 	}
 }
